f99a26a5a203d4c2abaf6d555029f2de41de7ac3
VEKozlov111 <vlad.kozlov.2015@gmail.com>
2023-03-23 23:20:10 +0300

    feat: add card component

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import Search from "components/Search";
+import data from "../assets/data.json"
+import Card from "components/Card";
 
 class Main extends React.Component {
   render() {
-    return <Search />;
+    return (
+      <div>
+        <Search />
+        <div className="flex gap-4 pt-2 flex-wrap">
+          {data.products.map((card) => (
+            <Card card={card} key={card.id} />
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,11 @@ export interface INavItemProps {
   title: string;
   to: string;
 }
+export interface ICard {
+  id: number;
+  title: string;
+  description: string;
+  brand: string;
+  category: string;
+  thumbnail: string;
+}
