423b94c6cd397a2ca6c75358fc714abcaa3f447d
VEKozlov111 <vlad.kozlov.2015@gmail.com>
2023-03-31 21:41:43 +0300

    feat: add search logic

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
+import data from "../assets/data.json";
+
+interface ISearchProps {
+  searchQuerry: string,
+  setSearchQuerry: React.Dispatch<React.SetStateAction<string>>
+};
 
 
-function Search() {
-
+function Search({ searchQuerry, setSearchQuerry }: ISearchProps) {
   return (
     <div className="">
       <form className="flex justify-center gap-4">
-        <input className="border rounded py-2 px-4" type="search" />
-        <button className="btn">Search</button>
+        <input className="border rounded py-2 px-4" type="search"
+          value={searchQuerry}
+          onChange={e => setSearchQuerry(e.target.value)}
+          placeholder="Search"
+        />
       </form>
     </div>
   );
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import Search from "components/Search";
 import data from "../assets/data.json";
 import Card from "components/Card";
 
 function Main() {
+  const [searchQuerry, setSearchQuerry] = useState('');
+  const search = useMemo(() => {
+    return data.products.filter(item => item.title.toLowerCase().includes(searchQuerry))
+  }, [searchQuerry]);
+
   return (
     <div>
-      <Search />
+      <Search searchQuerry={searchQuerry}
+        setSearchQuerry={setSearchQuerry}
+      />
       <div className="flex gap-4 pt-2 flex-wrap">
-        {data.products.map((card) => (
+        {search.map((card) => (
           <Card card={card} key={card.id} />
         ))}
       </div>
